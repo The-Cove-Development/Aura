3b44dc06a10eeceeb5e02a0dbce95ace0560c0de
The-Cove-Development <admin@theCovedev.com>
2025-04-08 01:31:00 -0400

    Added custom colors, fonts, and background for landing page

--- config.js
+++ config.js
@@ -2,13 +2,15 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-                
+                dark_lime_green: '#659B5E',
+                light_green: '#95BF74',
             },
             fontFamily: {
-                
+                sail: ['Sail', 'serif'],
+                plus_jakarta_sans: ['Plus Jakarta Sans', 'sans-serif'],
             },
             backgroundImage: {
-                
+                landing: "url('./images/landing.png')",
             }
         }
     }
